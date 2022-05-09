a6757c8e60355d05a76672c764fc977b1ae336ec
MahmoudEmawi7122 <56602265+MahmoudEmawi@users.noreply.github.com>
2022-05-09 18:49:10 +0300

    Main

---
+++ Blockchain.py
@@ -0,0 +1,58 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+
+        self.new_block(previous_hash="", proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.pending_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        return block
+
+    @property
+    def last_block(self):
+ 
+        return self.chain[-1]
+
+    def new_transaction(self, sender, recipient, amount):
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['index'] + 1
+
+
+    def hash(self, block):
+        string_object = json.dumps(block, sort_keys=True)
+        block_string = string_object.encode()
+
+        raw_hash = hashlib.sha256(block_string)
+        hex_hash = raw_hash.hexdigest()
+
+        return hex_hash
+
+
+blockchain = Blockchain()
+t1 = blockchain.new_transaction("Mahmoud", "Emawi", '7 BTC')
+t2 = blockchain.new_transaction("Adham", "Sami", '10 BTC')
+blockchain.new_block(12345)
+
+t3 = blockchain.new_transaction("Ahmed", "Sosy", '1 BTC')
+blockchain.new_block(6789)
+
+print("Block: ", blockchain.chain)
